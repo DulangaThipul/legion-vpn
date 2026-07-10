81a0419072dc9759bc35b5a31a0779ad5bd322fd
DulangaThipul <108794574+DulangaThipul@users.noreply.github.com>
2026-07-10 21:28:03 +0530

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,22 @@
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const prismaClientSingleton = () => {
+  // Updated Prism by Dulanga
+  return new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
+};
 
-// 🚀 Proxy එකක් පාවිච්චි කරලා Prisma එක Lazy Load කරනවා. 
-// මේකෙන් බිල්ඩ් වෙද්දී PrismaClient එක ක්‍රෑෂ් වෙන්නේ නැහැ, මොකද මේක වැඩ කරන්නේ ලයිව් රන් වෙද්දී විතරයි!
-export const prisma = new Proxy({} as PrismaClient, {
-  get: (target, prop) => {
-    if (!globalForPrisma.prisma) {
-      globalForPrisma.prisma = new PrismaClient();
-    }
-    return Reflect.get(globalForPrisma.prisma, prop);
-  },
-});
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
